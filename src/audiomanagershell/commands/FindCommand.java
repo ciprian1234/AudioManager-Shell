d00b088f203766130797659b887d5e43fa92af77
DoniciMarius <donicimariusflorian@gmail.com>
2016-03-23 01:43:10 +0200

    FindCommand am facut recursivitatea mai trebuie doar sa fac cautarea fisierului.
    InfoCommand trebuie sa gasesc metadatele cu Apache Tika.
    FavCommand doar scheletul.

--- src/audiomanagershell/commands/FindCommand.java
+++ src/audiomanagershell/commands/FindCommand.java
@@ -5,10 +5,55 @@
  */
 package audiomanagershell.commands;
 
+import audiomanagershell.commands.exceptions.CommandException;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author ciprian
  */
-public class FindCommand {
-    
+public class FindCommand extends Command{
+    public FindCommand(Path reference){
+        super(reference);
+    }
+
+    @Override
+    public void execute() throws CommandException{
+        Path currentFolder = Paths.get(this.pathRef.toString());
+        List<Path> files = new ArrayList<>();
+        try{
+            DirectoryStream<Path> stream = Files.newDirectoryStream(currentFolder);
+            for(Path file : stream)
+                files.add(file);
+            stream.close();
+
+
+            //Trebuie implementata cautarea
+            for(Path file : files) {
+                if (Files.isRegularFile(file))
+                    System.out.printf("file >");
+                if (Files.isDirectory(file)){
+                    Command recursiveFolder = new FindCommand(file);
+                    recursiveFolder.init(this.arg);
+                    recursiveFolder.execute();
+                }
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void init(String args) {
+        this.arg = args;
+    }
+
 }
